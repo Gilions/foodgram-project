f4bc261b6cf1b9a27c91dcb643eb2eae2d09d9f3
Vitaly Sofronyuk <Gilions@icloud.com>
2021-04-19 08:03:58 +0300

    remove static, media

--- foodgram/apps/recipes/static/shopList.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector('.shopping-list');
-const counterId = document.querySelector('#counter');
-const api = new Api(apiUrl);
-const header = new Header(counterId);
-
-const shopList = new ShopList(container, header, api);
-shopList.addEvent();
